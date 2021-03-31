e77c682c7efd5afbf402f9cafd0a0f1d68523dde
ap7 <arhouston7@gmail.com>
2021-03-31 15:50:27 +0200

    Setup mvvm project
    add some dependencies to implement MVVM architecture
    
    add view: LoginActivity.kt & SignUpActivity.kt

---
+++ app/src/main/java/com/softbankrobotics/mvvm/ui/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.softbankrobotics.mvvm.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.softbankrobotics.mvvm.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
